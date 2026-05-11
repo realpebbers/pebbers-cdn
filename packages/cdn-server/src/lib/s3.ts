978f5504a44f2ba888c6200d65dd55e01f471e59
Pebbers <peb@starlane.org>
2026-05-11 14:50:49 +0300

    fix: ACCESS_KEY_ID and SECRET_ACCESS_KEY only read if they are defined for local development

--- packages/cdn-server/src/lib/s3.ts
+++ packages/cdn-server/src/lib/s3.ts
@@ -6,10 +6,13 @@ import { UploadError } from "./errors";
 // TODO: different code for Lambda that uses IAM roles?
 export const s3Client = new S3Client({
   region: env.AWS_REGION,
-  credentials: {
-    accessKeyId: env.ACCESS_KEY_ID,
-    secretAccessKey: env.SECRET_ACCESS_KEY,
-  },
+  ...(env.ACCESS_KEY_ID &&
+    env.SECRET_ACCESS_KEY && {
+      credentials: {
+        accessKeyId: env.ACCESS_KEY_ID,
+        secretAccessKey: env.SECRET_ACCESS_KEY,
+      },
+    }),
 });
 
 export async function uploadFile(key: string, body: ReadableStream) {
