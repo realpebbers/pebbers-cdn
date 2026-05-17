b2057301c37a7be3e3ebccc342b13f547644c03d
Pebbers <3bdallah.emad@gmail.com>
2026-05-17 20:42:00 +0300

    refactor: began using shadcn/ui

--- packages/cdn-client/vite.config.ts
+++ packages/cdn-client/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -8,5 +9,5 @@ export default defineConfig({
 			localsConvention: "camelCaseOnly",
 		},
 	},
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 });
